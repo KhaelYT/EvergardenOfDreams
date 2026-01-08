90e48dd3508a2b9c42458aeeea13764a2622a39d
Khael <nunoalexsantos.9b@gmail.com>
2026-01-08 11:35:26 +0000

    Initial Commit

---
+++ src/main/java/net/khael/evergarden/EvergardenOfDreamsClient.java
@@ -0,0 +1,10 @@
+package net.khael.evergarden;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EvergardenOfDreamsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/khael/evergarden/EvergardenOfDreamsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.khael.evergarden;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EvergardenOfDreamsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
